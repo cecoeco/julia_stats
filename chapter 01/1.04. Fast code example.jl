170108dabb4d1477c77f815f5cdf3d91846288af
cecoeco <108555136+cecoeco@users.noreply.github.com>
2023-11-15 19:17:32 -0500

    Delete chapter 01 directory

--- chapter 01/1.04. Fast code example.jl
+++
@@ -1,7 +0,0 @@
-using Statistics
-
-@time begin
-    Data = [mean(rand(5*10^2)) for _ in 1:10^6]
-    println("98% of the means lie in the estimated range: ",
-                    (quantile(Data,0.01),quantile(Data,0.99)) )
-end
